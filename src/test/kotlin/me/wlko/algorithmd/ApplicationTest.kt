b1426c38e4321fe9ccd0379d6a7955e31d1491d3
Alex Kovrigin <a.kovrigin0@gmail.com>
2021-07-13 15:34:11 +0300

    fix tests

--- src/test/kotlin/me/wlko/algorithmd/ApplicationTest.kt
+++ src/test/kotlin/me/wlko/algorithmd/ApplicationTest.kt
@@ -12,6 +12,7 @@ import io.ktor.features.*
 import org.slf4j.event.*
 import io.ktor.gson.*
 import io.ktor.application.*
+import io.ktor.config.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.server.testing.*
@@ -19,10 +20,26 @@ import me.wlko.algorithmd.plugins.*
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
+fun ApplicationEnvironment.config(values: Map<String, String>) {
+    (config as MapApplicationConfig).apply {
+        values.forEach { (path, value) ->
+            put(path, value)
+        }
+    }
+}
+
 class ApplicationTest {
     @Test
     fun testRoot() {
-        withTestApplication({ configureRouting() }) {
+        withTestApplication({
+            environment.config(mapOf(
+                "jwt.audience" to "https://api.algorithmd.wlko.me",
+                "jwt.domain" to "https://algorithmd.eu.auth0.com/",
+                "jwt.realm" to "Test server"
+            ))
+            configureSecurity()
+            configureRouting()
+        }) {
             handleRequest(HttpMethod.Get, "/v1/health").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 assertEquals("OK", response.content)
